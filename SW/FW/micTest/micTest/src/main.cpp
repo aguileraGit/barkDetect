c0ff7cb78bbee4589ccf1ee8edf3987da00078da
Diego Aguilera <diego.aguilera@variableinc.com>
2018-07-11 22:20:49 -0400

    Updated a bunch of references. Need to figure out the registers.

--- SW/FW/micTest/micTest/src/main.cpp
+++ SW/FW/micTest/micTest/src/main.cpp
@@ -19,6 +19,51 @@ https://github.com/PaulStoffregen/Audio/blob/master/input_adc.cpp
 I believe line 66 sets the ADC sampling speed
 
 Work on setting slower sampling rate
+
+From input_adc
+https://github.com/PaulStoffregen/Audio/blob/master/input_adc.cpp
+
+uC Reference
+https://www.nxp.com/docs/en/reference-manual/K66P144M180SF5RMV2.pdf
+
+// set the programmable delay block to trigger the ADC at 44.1 kHz
+#if defined(KINETISK)
+if (!(SIM_SCGC6 & SIM_SCGC6_PDB)
+  || (PDB0_SC & PDB_CONFIG) != PDB_CONFIG
+  || PDB0_MOD != PDB_PERIOD
+  || PDB0_IDLY != 1
+  || PDB0_CH0C1 != 0x0101) {
+
+  //Start config
+  SIM_SCGC6 |= SIM_SCGC6_PDB;
+    Pretty sure this enables PDB or ADC. Need to find it in other h/cpp files
+
+  PDB0_IDLY = 1;
+    PDB Interrupt Delay
+    Specifies the delay value to schedule the PDB interrupt. It can be used to schedule an independent
+    interrupt at some point in the PDB cycle. If enabled, a PDB interrupt is generated, when the counter is
+    equal to the IDLY. Reading this field returns the value of internal register that is effective for the current
+    cycle of the PDB.
+
+  PDB0_MOD = PDB_PERIOD;
+    Specifies the period of the counter. When the counter reaches this value, it will be reset back to zero. If the
+    PDB is in Continuous mode, the count begins anew. Reading this field returns the value of the internal
+    register that is effective for the current cycle of PDB.
+
+    PDB_PERIOD defined in utilities/pdb.h
+       #define PDB_CONFIG (PDB_SC_TRGSEL(15) | PDB_SC_PDBEN | PDB_SC_CONT | PDB_SC_PDBIE | PDB_SC_DMAEN)
+       #if F_BUS == 120000000
+         #define PDB_PERIOD (2720-1)
+
+  PDB0_SC = PDB_CONFIG | PDB_SC_LDOK;
+    Status and Control Register
+
+  PDB0_SC = PDB_CONFIG | PDB_SC_SWTRIG;
+  PDB0_CH0C1 = 0x0101;
+}
+#endif
+
+
 */
 
 // GUItool: begin automatically generated code
